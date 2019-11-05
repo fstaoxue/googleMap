3733f644609aa56b7b066489690a1b61e7a58fb0
fstaoxue <1479583767@qq.com>
2019-11-05 21:59:09 +0800

    taoge  test

---
+++ src/main/java/com/taoge/googlemap/GooglemapApplication.java
@@ -0,0 +1,13 @@
+package com.taoge.googlemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglemapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooglemapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taoge/googlemap/GooglemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoge.googlemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglemapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
